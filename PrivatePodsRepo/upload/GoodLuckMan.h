49bdefa557aa2786635873cae7577b82849eff4d
XpSun <teamwork_SunXp@163.com>
2018-07-11 10:40:32 +0800

    add

---
+++ PrivatePodsRepo/upload/GoodLuckMan.h
@@ -0,0 +1,13 @@
+//
+//  GoodLuckMan.h
+//  PrivatePodsRepo
+//
+//  Created by WorkSpace_Sun on 2018/7/11.
+//  Copyright © 2018年 WorkSpace_Sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoodLuckMan : NSObject
+
+@end
